1824fc31279affa61d8e38d72b26f1652ab38277
Ofir Matasas <ofir.matasas@gmail.com>
2022-04-06 16:25:23 +0300

    Moving the old homepage to the "about" section
    The old homepage became the "about" section of the website,
    so the new homepage, when it will be created, will take it's place.
    
    Signed-off-by: Ofir Matasas <ofir.matasas@gmail.com>

--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'main/homepage.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
