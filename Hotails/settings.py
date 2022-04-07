18d02e4550b28c37c42b54709d8d4b9e9c09a6aa
tamirmatok <tamirmatok123@gmail.com>
2022-04-07 11:51:41 +0300

    Add login and log-out functionallity.
    Adding a landing page that is a login page to the Hotails website.
    In order for a user to be able to access the site,
    he must be a user and enter his details on this page.
    After sucsessful log-in the application recognize the user with all of his details.

--- Hotails/settings.py
+++ Hotails/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'review.apps.ReviewConfig',
     'orders.apps.OrdersConfig',
     'message.apps.MessageConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
