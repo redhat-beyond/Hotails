82de0b23ab1a97cdb02b6f87ff1c1fe126266b14
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-04-27 01:15:22 +0300

    Add average rating to the daycare profile
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- daycare/views.py
+++ daycare/views.py
@@ -9,7 +9,7 @@ def daycare_home(request):
     context = {
         'daycare': request.user.daycare,
         'reviews': Review.get_review_by_daycare_id(request.user.daycare.id),
-        'images': Image.get_images_by_daycare_id(request.user.daycare.id)
+        'images': Image.get_images_by_daycare_id(request.user.daycare.id),
+        'rating': Review.get_average_rating_by_daycare_id(request.user.daycare.id)
     }
-
     return render(request, 'daycare/daycare-homepage.html', context)
