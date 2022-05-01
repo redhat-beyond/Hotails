fa7a2f04597fb37fa5da8420c921f9f67f20a40f
Erez Cohen <erez6356458@gmail.com>
2022-05-01 09:11:01 +0300

    Fixing login tests:
    After changing the homepage of the site, changes are required in user login tests.
    So the tests test a daycare user's login to his relevant homepage with the appropriate details,
    and a dogowner user test will be performed in another PR that is responsible for creating a dogowner user homepage
    
    Signed-off-by: Erez Cohen <erez6356458@gmail.com>

--- main/views.py
+++ main/views.py
@@ -3,19 +3,21 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 from daycare.models import DayCare
+from dogowner.models import DogOwner
 from daycare.views import daycare_home
 
 
 def index(request):
     if request.user.is_authenticated:
-        if DayCare.objects.filter(user=request.user).exists():
-            return redirect(to='homepage')
+        return redirect(to='homepage')
     return redirect(to='login')
 
 
 @login_required()
 def homepage(request):
-    if DayCare.objects.filter(user=request.user).exists():
+    if DogOwner.objects.filter(user=request.user).exists():
+        return render(request, 'main/homepage.html')
+    elif DayCare.objects.filter(user=request.user).exists():
         return daycare_home(request)
 
 
